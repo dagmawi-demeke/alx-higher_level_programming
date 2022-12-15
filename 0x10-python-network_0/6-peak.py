14a05a2a01b3da89fa5a93b31ff1201f29b9f2b6
dagmawi-demeke <67114822+dagmawi-demeke@users.noreply.github.com>
2022-12-15 20:55:18 +0300

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
-"""contains the function find_peak"""
+"""This is the peak module."""
 
 
 def find_peak(list_of_integers):
-    """finds a peak in a list of unsorted integers"""
-    li = list_of_integers
-    l = len(li)
-    if l == 0:
-        return
-    m = l // 2
-    if (m == l - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
-        return li[m]
-    if m != l - 1 and li[m + 1] > li[m]:
-        return find_peak(li[m + 1:])
-    return find_peak(li[:m])
+    """Finds a peak in a list of unsorted integers.
+    Args:
+        list_of_integers (list): the list of integers to find the peak in.
+    """
+
+    if list_of_integers == []:
+        return None
+
+    length = len(list_of_integers)
+    if length == 1:
+        return list_of_integers[0]
+    if length == 2:
+        return max(list_of_integers)
+
+    half = int(length / 2)
+    peak = list_of_integers[half]
+    if peak > list_of_integers[half - 1] and peak > list_of_integers[half + 1]:
+        return peak
+    elif peak > list_of_integers[half + 1]:
+        return find_peak(list_of_integers[:half])
+    else:
+        return find_peak(list_of_integers[half + 1:])
