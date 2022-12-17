3a6b7bb1fc12309b77401ae5a42a9be5066606fc
dagmawi-demeke <67114822+dagmawi-demeke@users.noreply.github.com>
2022-12-17 13:07:12 +0300

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
-import requests
+"""Lists the 10 most recent commits on a given GitHub repository.
+Usage: ./100-github_commits.py <repository name> <repository owner>
+"""
 import sys
+import requests
 
-# Get repository name and owner name from command line arguments
-repo_name = sys.argv[1]
-owner_name = sys.argv[2]
 
-# Make API call to get commits
-url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
-response = requests.get(url)
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-# Print commit information
-for commit in response.json():
-  sha = commit['sha']
-  author = commit['commit']['author']['name']
-  print(f"{sha}: {author}")
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
